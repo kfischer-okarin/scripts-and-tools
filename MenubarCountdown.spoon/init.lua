48f0a68a6c8f62cf5d2462b01851b91738c87827
Kevin Fischer <kevin@agileware.jp>
2022-06-25 17:04:38 +0900

    Rewrite MenubarCountdown to lua class

--- MenubarCountdown.spoon/init.lua
+++ MenubarCountdown.spoon/init.lua
@@ -7,51 +7,56 @@ local MenubarCountdown = {
 }
 
 function MenubarCountdown.new(label, endTime, options)
-  local countdown = {}
+  local countdown = {
+    label = label,
+    endTime = endTime,
+  }
   if not options then
     options = {}
   end
+  countdown.onFinish = options.onFinish
 
-  function countdown:start()
-    countdown.menu = hs.menubar.new()
-    countdown.endTime = endTime
-    countdown.timer = hs.timer.doEvery(1, function()
-      countdown:refreshUI()
-      if (self:getRemainingTime() == 0) then
-        if options.onFinish then
-          options.onFinish()
-        end
-      end
-    end)
-    countdown.onFinish = options.onFinish
-
-    countdown:refreshUI()
-  end
-
-  function countdown:refreshUI()
-    remainingTime = self:getRemainingTime()
-
-    minutes = math.floor(remainingTime / 60)
-    hours = math.floor(minutes / 60)
-    minutes = minutes % 60
-    seconds = remainingTime % 60
-    if hours > 0 then
-      self.menu:setTitle(label .. ': ' .. string.format('%d:%02d:%02d', hours, minutes, seconds))
-    else
-      self.menu:setTitle(label .. ': ' .. string.format('%d:%02d', minutes, seconds))
-    end
-  end
-
-  function countdown:getRemainingTime()
-    return self.endTime - os.time()
-  end
-
-  function countdown:stop()
-    self.menu:delete()
-    self.timer:stop()
-  end
+  setmetatable(countdown, MenubarCountdown)
+  MenubarCountdown.__index = MenubarCountdown
 
   return countdown
+end
+
+function MenubarCountdown:start()
+  self.menu = hs.menubar.new()
+  self.timer = hs.timer.doEvery(1, function()
+    self:refreshUI()
+    if (self:getRemainingTime() == 0) then
+      if self.onFinish then
+        self.onFinish()
+      end
+    end
+  end)
+
+  self:refreshUI()
+end
+
+function MenubarCountdown:refreshUI()
+  remainingTime = self:getRemainingTime()
+
+  minutes = math.floor(remainingTime / 60)
+    hours = math.floor(minutes / 60)
+  minutes = minutes % 60
+  seconds = remainingTime % 60
+  if hours > 0 then
+    self.menu:setTitle(self.label .. ': ' .. string.format('%d:%02d:%02d', hours, minutes, seconds))
+  else
+    self.menu:setTitle(self.label .. ': ' .. string.format('%d:%02d', minutes, seconds))
+  end
+end
+
+function MenubarCountdown:getRemainingTime()
+  return self.endTime - os.time()
+end
+
+function MenubarCountdown:stop()
+  self.menu:delete()
+  self.timer:stop()
 end
 
 return MenubarCountdown
