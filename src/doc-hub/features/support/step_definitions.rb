4ad0766d91314796eec695e42f9839b760469052
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-07-13 08:40:28 +0900

    Fix step definitions

--- src/doc-hub/features/support/step_definitions.rb
+++ src/doc-hub/features/support/step_definitions.rb
@@ -1,13 +1,13 @@
-Given (/^I do not set the environment variable "(.+)"$/) do |var|
+Given(/^I do not set the environment variable "(.+)"$/) do |var|
   delete_environment_variable(var)
 end
 
-When (/^I add documentation with id "([^"]*)"$/) do |doc_id|
+When(/^I add documentation with id "([^"]*)"$/) do |doc_id|
   fixture_url = "https://specifications.freedesktop.org/basedir-spec/latest/"
   step %(I successfully run `doc-hub add #{doc_id} #{fixture_url}`)
 end
 
-Then (/^the directory "([^"]*)" should contain a downloaded documentation$/) do |dir_path|
+Then(/^the directory "([^"]*)" should contain a downloaded documentation$/) do |dir_path|
   step %(the directory "#{dir_path}" should exist)
   step %(a file "#{dir_path}/index.md" should exist)
 end
