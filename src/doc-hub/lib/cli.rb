2dc3941a7a3cbbfe6e84600bfc0aec214746bf6a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-07-13 22:20:12 +0900

    Remove dummy commands

--- src/doc-hub/lib/cli.rb
+++ src/doc-hub/lib/cli.rb
@@ -6,21 +6,6 @@ require 'fileutils'
 require 'thor'
 
 class CLI < Thor
-  desc "hello NAME", "Say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}!"
-  end
-
-  desc "goodbye NAME", "Say goodbye to NAME"
-  def goodbye(name)
-    puts "Goodbye #{name}!"
-  end
-
-  desc "version", "Show version"
-  def version
-    puts "doc-hub v1.0.0"
-  end
-
   desc "add NAME URL", "Add documentation from URL"
   def add(name, url)
     doc_path = File.join(storage_path, name)
