2b1bb7ede5fdde2dfebeb99904cede2c4be3ae34
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-12-10 13:58:24 +0900

    Month statistics

--- src/work-time/lib/worktime/tracker.rb
+++ src/work-time/lib/worktime/tracker.rb
@@ -10,12 +10,15 @@ module Worktime
   class NotWorkingError < StandardError; end
 
   class Tracker
-    Status = Data.define(:state, :work_minutes)
+    Status = Data.define(:state, :work_minutes, :surplus_minutes)
+    DayStats = Data.define(:date, :work_minutes, :surplus_minutes)
+    MonthStats = Data.define(:days, :total_surplus_minutes)
 
     def initialize(data_dir:, now: Time.now)
       @data_dir = data_dir
       @now = now
       load_events
+      load_overrides
     end
 
     def start
@@ -44,36 +47,68 @@ module Worktime
     end
 
     def status
-      Status.new(state: state, work_minutes: work_minutes)
+      Status.new(state: state, work_minutes: work_minutes, surplus_minutes: surplus_minutes)
+    end
+
+    def set_hours(hours, date: @now.to_date)
+      @overrides[date] = hours
+      save_overrides
+    end
+
+    def month_statistics
+      dates = @events.map { |e| e[:date] }.uniq.sort
+      days = dates.map do |date|
+        work_mins = work_minutes_for_date(date)
+        expected_mins = expected_minutes_for_date(date)
+        DayStats.new(date: date, work_minutes: work_mins, surplus_minutes: work_mins - expected_mins)
+      end
+      MonthStats.new(days: days, total_surplus_minutes: days.sum(&:surplus_minutes))
     end
 
     private
 
+    def surplus_minutes
+      work_minutes - expected_minutes
+    end
+
+    def expected_minutes
+      expected_minutes_for_date(@now.to_date)
+    end
+
+    def expected_minutes_for_date(date)
+      hours = @overrides.fetch(date, 8)
+      hours * 60
+    end
+
     def work_minutes
-      events = events_for_date(@now.to_date)
+      work_minutes_for_date(@now.to_date)
+    end
+
+    def work_minutes_for_date(date)
+      events = events_for_date(date)
       return 0 if events.empty?
 
       start_event = events.find { |e| e[:event] == :start }
       stop_event = events.find { |e| e[:event] == :stop }
       return 0 unless start_event && stop_event
 
-      start_time = parse_time(start_event[:time])
-      stop_time = parse_time(stop_event[:time])
+      start_time = parse_time_for_date(start_event[:time], date)
+      stop_time = parse_time_for_date(stop_event[:time], date)
       total = ((stop_time - start_time) / 60).to_i
 
-      total - break_minutes(events)
+      total - break_minutes_for_date(events, date)
     end
 
-    def break_minutes(events)
+    def break_minutes_for_date(events, date)
       total = 0
       break_start = nil
 
       events.each do |event|
         case event[:event]
         when :break_start, :lunch_start
-          break_start = parse_time(event[:time])
+          break_start = parse_time_for_date(event[:time], date)
         when :break_end, :lunch_end
-          total += ((parse_time(event[:time]) - break_start) / 60).to_i if break_start
+          total += ((parse_time_for_date(event[:time], date) - break_start) / 60).to_i if break_start
           break_start = nil
         end
       end
@@ -81,9 +116,9 @@ module Worktime
       total
     end
 
-    def parse_time(time_str)
+    def parse_time_for_date(time_str, date)
       hour, min = time_str.split(":").map(&:to_i)
-      Time.new(@now.year, @now.month, @now.day, hour, min, 0)
+      Time.new(date.year, date.month, date.day, hour, min, 0)
     end
 
     def state
@@ -136,6 +171,29 @@ module Worktime
         csv << %w[date event time]
         @events.each do |e|
           csv << [e[:date], e[:event], e[:time]]
+        end
+      end
+    end
+
+    def overrides_path
+      File.join(@data_dir, "#{@now.strftime('%Y-%m')}-worktime-overrides.csv")
+    end
+
+    def load_overrides
+      @overrides = {}
+      return unless File.exist?(overrides_path)
+
+      CSV.foreach(overrides_path, headers: true, header_converters: :symbol) do |row|
+        @overrides[Date.parse(row[:date])] = row[:expected_hours].to_i
+      end
+    end
+
+    def save_overrides
+      FileUtils.mkdir_p(@data_dir)
+      CSV.open(overrides_path, "w") do |csv|
+        csv << %w[date expected_hours]
+        @overrides.each do |date, hours|
+          csv << [date, hours]
         end
       end
     end
