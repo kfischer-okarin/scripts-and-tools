5b6d741bb54a19b806c590d65d9cda0fa4620c9d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-12-28 14:19:54 +0900

    Setup joplin cli fodler

---
+++ src/joplin/test/test_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "webmock/minitest"
+require_relative "../lib/joplin"
+
+module Joplin
+  class TestCase < Minitest::Test
+  end
+end
